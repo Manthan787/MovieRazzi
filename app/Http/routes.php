79a49d61edc6d44369d10bee4b35fd07cc1f710d
manthan787 <manthant15@gmail.com>
2015-06-21 08:30:46 +0530

    Updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/', function(){
+   return 'HI!';
+});
+
 Route::get('/api/detect', function() {
    $detector = new MovieRazzi\Services\FaceRecognition\Detector("6d46f8babc494ad293d8501a8d4cd099","af3a4243153e475a9ff95a6b186b67b3");
    $detector->fun();
